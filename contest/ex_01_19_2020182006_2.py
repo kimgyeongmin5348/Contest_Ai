8fb52ac81e8327127171ac2a3e4cef415b714b33
kimgyeongmin5348 <135432080+kimgyeongmin5348@users.noreply.github.com>
2024-11-19 16:25:08 +0900

    알고리즘 과제(Chained Matrix Multiplication) 19-2 과제 완 교수님이 보여준 답이랑 똑같이 나옴

--- contest/ex_01_19_2020182006_2.py
+++ contest/ex_01_19_2020182006_2.py
@@ -1,27 +1,31 @@
 d = [2,5,9,3,7,2,9,3,8,2,3]
 
-
-mc = len(d) -1
+mc = len(d) - 1
 
 INF = float('inf')
 
-C = [[??? for ??? range(mc+1)]]
-P =
+C = [[INF for j in range(mc+1)] for i in range(mc+1)]
+P = [[0 for j in range(mc+1)] for i in range(mc+1)]
 
-for sps in range(2, mc+1): # sub_problem_size
-    spc = ???   #sub_problem_count
+for i in range(1, mc+1):
+    C[i][i] = 0
+
+for sps in range(2, mc+1):  # sub_problem_size
+    spc = mc - sps + 1      # sub_problem_count
     for beg in range(1, spc+1):
-        end = ???  # 예를 들면 sps =3, sps = 4 , end = 6
-        ???
-        ???
+        end = beg + sps - 1  # 예를 들면 sps=3, beg=4, end=6
         for k in range(beg, end):
-            # 최소값 갱신
+            cost = C[beg][k] + C[k+1][end] + d[beg-1]*d[k]*d[end]
+            if cost < C[beg][end]:
+                C[beg][end] = cost
+                P[beg][end] = k
 
-def path(i,j):
-    if ???:
+def path(i, j):
+    if i == j:
         return f'A{i}'
-    ???
+    k = P[i][j]
+    a = path(i, k)
+    b = path(k+1, j)
     return f'({a}x{b})'
-
 
 print(path(1, mc), C[1][mc])
